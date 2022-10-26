f693255dc648757ac3b461312ae83af79569ba6d
S-korobka <111728423+S-korobka@users.noreply.github.com>
2022-10-26 23:12:18 +0300

    DZ23

---
+++ src/test/java/org/example/hooks/DriverHooks.java
@@ -0,0 +1,17 @@
+package org.example.hooks;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.example.driver.DriverManager;
+
+public class DriverHooks {
+    @Before
+    public void setupDriver(){
+        DriverManager.setupDriver();
+    }
+
+    @After
+    public void quit(){
+        DriverManager.quitDriver();
+    }
+}
